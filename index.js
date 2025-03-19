2cb26fd9b1a04852cfb76eb3dc725145d507f829
samuel8772 <kamauskk005@gmail.com>
2025-03-19 10:00:28 +0300

    Update complete Code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Sample ramen data
 const ramens = [
     { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg", rating: 5, comment: "Rich and creamy!" }
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "kojiro.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "gyukotsu.jpg", rating: 5, comment: "Rich and creamy!" }
 ];
 
 // Function to display ramen images
